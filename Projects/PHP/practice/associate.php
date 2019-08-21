6873686a05a358a4a12a94fba3b125f1eca7c079
Eli <rouninlopez@gmail.com>
2019-08-21 16:53:23 -0500

    practiced some php includes and group includes

---
+++ Projects/PHP/practice/associate.php
@@ -0,0 +1,27 @@
+<?php
+define(strict=1);
+namespace Files/Associates;
+
+use '\Applications\Tools'
+{
+	Date,
+	Time,
+	Math,
+	
+}
+
+class Associate
+{
+	private Date Today=new Today();
+	Time timestamp =new Time();
+	
+	static storeNumber=456;
+	
+	
+	
+	
+	
+}
+
+
+?>
---
+++ Projects/PHP/practice/proccessmachine.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Applications\associates
+
+include __DIR__ . '/Applications\associates';
+
+using namespace Applications\associates;
+
+class proccessmachine
+{
+	
+	Associate ass=new Associate():
+	
+	Associate ass::storeNumber;
+	
+	public static makeAssociate(Associate $ass)
+	{
+		
+		
+	}
+}
+?>
