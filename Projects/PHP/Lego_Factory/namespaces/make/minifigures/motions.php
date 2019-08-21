a9f69f05229276595cb823bba33d1088a0999738
Eli <rouninlopez@gmail.com>
2019-08-21 16:25:07 -0500

    added custom classes to paint a lego to a particular unit

---
+++ Projects/PHP/Lego_Factory/namespaces/make/minifigures/motions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace //minifigure
+
+require '/CNC/motions.php'
+using namespace motions;
+
+Interface
+{
+	public moveRightArm();
+	public moveLeftArm();
+	
+	public calculateMass();
+	
+	
+	
+	
+}
+
+
+
+
+
+?>
