c98231d7918a9de8d4993dbc21e3f82a00d08ae4
Elijah Lopez <rouninlopez@gmail.com>
2019-08-20 20:26:16 -0500

    add new files, notes and a PHP practice project

---
+++ Projects/PHP/Lego_Factory/namespaces/make/minifigures/std_lego.php
@@ -0,0 +1,48 @@
+<?php
+	namespace //minifigures
+	
+	require '/Parts/Lego/Minifigure';
+	using namespace Minifigure;
+	
+	abstract class std_lego()
+	{
+		
+		
+		public Minifigure head;
+		public Minifigure torso;
+		public Minifigure legs;
+		public Minifigure left_arm;
+		public Minifigure right_arm;
+		public Minifigure left_hand;
+		public Minifigure right_hand;
+	
+	
+	
+		//Make//
+		public function makeArm()
+		{
+			
+		}
+		 public function makeHead()
+		{
+			
+		}
+		
+		//Setters//
+		 public set_head()
+		{
+				
+		}
+		public get_head()
+		{
+			
+			
+		}
+		
+		//Getters//
+		
+		//Doers//
+		
+	}
+
+?>
